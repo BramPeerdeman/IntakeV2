d15ad327e93f0be3a7940a70b27d59d1fda6eb2c
Nikhil Jishtu <95859662+NikhilJishtu@users.noreply.github.com>
2023-05-30 14:39:59 +0200

    commit

--- student_overzicht/studenten/studenten.php
+++ student_overzicht/studenten/studenten.php
@@ -1,12 +1,9 @@
 <?php
-
 require 'config.php';
-
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Table with database</title>
     <style>
@@ -25,7 +22,7 @@ require 'config.php';
         }
 
         tr:nth-child(even) {
-            background-color: #f2f2f2
+            background-color: #f2f2f2;
         }
     </style>
 </head>
@@ -36,23 +33,34 @@ require 'config.php';
             <th>naam</th>
             <th>geslacht</th>
             <th>id</th>
+            <th>intake</th>
+            <th>opdracht</th>
+            <th>beoordeeld</th>
+            <th>score</th>
         </tr>
         <?php
-        $sql = "SELECT Voornaam, Geslacht, StudentID FROM Persoon_inschrijving";
-        $result = $conn->query($sql);
+        $sql = "SELECT p.Voornaam, p.Geslacht, p.StudentID, s.Intake_rondeID, o.Naam, s.Gesprek_Software_Development_YN, s.Score
+                FROM Persoon_inschrijving p
+                JOIN Score s ON p.StudentID = s.StudentID
+                JOIN Opdracht o ON s.Opdracht_Id = o.OpdrachtID";
+        $result = $mysqli->query($sql);
+        if (!$result) {
+            echo "Error: " . $mysqli->error;
+            // Handle the error appropriately
+        }
         if ($result->num_rows > 0) {
             // output data of each row
             while ($row = $result->fetch_assoc()) {
                 echo "<tr><td>" . $row["Voornaam"] . "</td><td>" . $row["Geslacht"] . "</td><td>"
-                    . $row["StudentID"] . "</td></tr>";
+                    . $row["StudentID"] . "</td><td>" . $row["Intake_rondeID"] . "</td><td>" . $row["Naam"] . "</td><td>"
+                    . $row["Gesprek_Software_Development_YN"] . "</td><td>" . $row["Score"] . "</td></tr>";
             }
             echo "</table>";
         } else {
             echo "0 results";
         }
-        $conn->close();
+        $mysqli->close();
         ?>
     </table>
 </body>
-
 </html>
